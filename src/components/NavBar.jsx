a6b0c0700edb022165abfc370a3a1772783f6844
Mohammed <naurbaev2020@gmail.com>
2023-04-27 14:27:39 +0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="">
+      <h1 className="flex text-3xl font-bold underline">Hello world!</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
